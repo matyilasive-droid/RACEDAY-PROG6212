b8ceabcc1afb7c631066923a79c3061248dd0582
matyilasive-droid <matyilasive@gmail.com>
2026-09-04 17:23:07 +0200

    Add User table
    Create the User entity with user details and roles

--- SCHEMA.sql
+++ SCHEMA.sql
@@ -1,1 +1,16 @@
 CREATE DATABASE RaceDay_PROG6212;
+CREATE TABLE Users(
+    UserID INT IDENTITY(1,1) NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
+    LastName NVARCHAR(50) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    PasswordHash VARCHAR(255) NOT NULL,
+    Role NVARCHAR(30) NOT NULL
+        CONSTRAINT CK_Users_Role
+        CHECK (Role IN (N'Organiser', N'Participant', N'Admin')),
+    CreatedAt DATETIME2(0) NOT NULL
+        CONSTRAINT DF_Users_CreatedAt DEFAULT (SYSDATETIME()),
+
+    CONSTRAINT PK_Users PRIMARY KEY (UserID),
+    CONSTRAINT UQ_Users_Email UNIQUE (Email)
+);
